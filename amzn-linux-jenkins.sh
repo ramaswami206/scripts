c9c418f78ff921a93f1908ec7fb6d3b4d2557942
ramaswami206 <33023741+ramaswami206@users.noreply.github.com>
2024-12-16 05:39:40 +0530

    Update amzn-linux-jenkins.sh

--- amzn-linux-jenkins.sh
+++ amzn-linux-jenkins.sh
@@ -28,3 +28,13 @@ echo "Initial admin password: $PASSWORD"
 
 # Print Jenkins URL
 echo "Jenkins URL: http://localhost:8080"
+
+# Install Terraform
+echo "Installing Terraform..."
+sudo yum install -y yum-utils
+sudo yum-config-manager --add-repo https://rpm.releases.hashicorp.com/AmazonLinux/hashicorp.repo
+sudo yum -y install terraform
+
+# Verify Terraform installation
+echo "Verifying Terraform installation..."
+terraform --version
